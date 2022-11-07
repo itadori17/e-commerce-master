01c48de8d59f6d1d55695f4bdb589e52db576838
Bongani <bonganicele806@gmail.com>
2022-11-07 17:25:07 +0200

    Hero

---
+++ .history/src/Components/Hero_20221107171917.js
@@ -0,0 +1,1 @@
+src/Components/Hero.js
---
+++ src/Components/Hero.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Hero.css'
+
+export default function Hero() {
+  return (
+    <>
+      <div className='hero-container'>
+            <div className='socials-list'>Socials</div>
+            <div className='slider'>Slider</div>
+            <div className='tags'>Tags</div>
+      </div>
+    </>
+  )
+}
